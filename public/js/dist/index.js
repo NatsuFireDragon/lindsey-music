ea4499c8ddc7a0df69e77268368c8861a5667e1a
Natsu <kirito.flamedragon@gmail.com>
2020-03-11 04:16:29 -0500

    added function to close menu when link is clicked

--- public/js/dist/index.js
+++ public/js/dist/index.js
@@ -1,6 +1,7 @@
 let menuBars = document.querySelector('.menu-bars');
 let mobileMenu = document.querySelector('.box');
 let close = document.querySelector('.close-btn');
+let listItems = document.getElementsByTagName('li');
 
 menuBars.addEventListener('click', function(){
   mobileMenu.classList.add('active');
@@ -9,3 +10,10 @@ menuBars.addEventListener('click', function(){
 close.addEventListener('click', function(){
   mobileMenu.classList.remove('active');
 })
+
+
+for(let i=0; i<listItems.length; i++){
+  listItems[i].addEventListener('click', function(){
+    mobileMenu.classList.remove('active');
+  })
+}
